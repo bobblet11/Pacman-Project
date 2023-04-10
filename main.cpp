18783a6b9572da18dcc39f2a36b034eef8a0079a
NAShaan <nashaan@conect.hku.hk>
2023-04-11 06:45:26 +0800

    reformated main and fixed the win state.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "YellowGhost.h"
 #include "PinkGhost.h"
 #include "CyanGhost.h"
+
 //colour pairs
 #define WALL_COLOUR 2
 
@@ -21,32 +22,10 @@
 
 const int FRAMERATE = 60;
 
-    //since gameObjects have the neccesary functions that screen uses, i can pass a pointer thru to screen
-
-
-    //initalise
-        //make all game objects, variables, states and mem allocs
-    //game loops
-        //do a check for the current game state
-        //if in game mode:
-            //check for any input, on another thread?
-            //perform a input check, see if the instruction is valid and doabel ie x, y check
-            //do a collectable check, see if any pills need to be deleted and points need to be modified
-            //do a ghost check, see if any ghosts are in way of character
-            //update position and anim state of character
-                //if game is ended beacuse of last pill or death
-                    //commence ending procedure
-            //if game is still continuing
-            //do a ghost path findiing check
-            //update all ghost positions and states
-
-            //do a render process, update all objects (?that have had a change) into the frame
-            //render the frame
-            //wait frame rate
-
 int main(int argc, char *argv[])
 {
     bool running = true;
+
     std::vector<GameObject*> handle;
     
     PlayableMap map(handle);
@@ -71,22 +50,19 @@ int main(int argc, char *argv[])
     game_obj_ptr = new Character("CharacterSprites.txt",screen.getWidth(), screen.getHeight(), map, 2,2, CHARACTER);
     handle.push_back(game_obj_ptr);
 
-        
 
-
-
-        //terminal setup
     cbreak();
     noecho();
     setlocale(LC_ALL,"");
     initscr();
     nodelay(stdscr,TRUE);
+
     if(has_colors() == FALSE)
 	{	endwin();
 		printf("Your terminal does not support color\n");
 		exit(1);
 	}
-	start_color();			/* Start color 			*/
+	start_color();
 
     //colour init
     init_pair(CHARACTER, COLOR_YELLOW,COLOR_BLACK);
@@ -101,6 +77,7 @@ int main(int argc, char *argv[])
     while(running)
     {
         auto start = std::chrono::steady_clock::now();
+
         handle.at(character_index)->handleCharacterMove(handle, character_index);
         handle.at(character_index)->updateAnimationState();
 
@@ -111,10 +88,14 @@ int main(int argc, char *argv[])
                 handle.at(i)->handleState();
             }
         }
-        if ( handle.size() == 1)
+
+        if ( handle.size() == 5)
         {
-            delete handle.at(character_index);
-            handle.erase(handle.begin()+character_index);
+            for (int i = 0; i < handle.size(); i++)
+            {
+                delete handle.at(i);
+                handle.erase(handle.begin()+i);
+            }
             break;
         }
         screen.render(handle);
@@ -122,9 +103,9 @@ int main(int argc, char *argv[])
 
         std::this_thread::sleep_for(std::chrono::milliseconds((1000/FRAMERATE) - std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()));
 
-
-
     }
+
+    //add win screen using ncurses here
     endwin();
     std::cout << "YOU WIN!" << std::endl;
 }
